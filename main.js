b366f70b1ac215b3807d8c90692f79c11e01d62e
Phanuwat_emotin <phanuwat_panya@elearning.cmu.ac.th>
2022-07-17 00:04:32 +0700

    5.5

--- main.js
+++ main.js
@@ -2,44 +2,38 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
-            brand: 'SE 331',
-            //image: './assets/images/socks_green.jpg',
-            //inStock: true,
+            brand: 'SE331',
+            // image: './assets/images/socks_green.jpg',
+            // inStock: true,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
+            variants: [{
+                    id: 2234,
+                    color: 'green',
+                    image: './assets/images/socks_green.jpg',
+                    quantity: 50
+                },
+                {
+                    id: 2235,
+                    color: 'blue',
+                    image: './assets/images/socks_blue.jpg',
+                    quantity: 50
+                }
             ],
-            selectedVariant: 0,
-            cart: 0,
+            cart: [0, 0],
+            SelectedVariant: 0,
             onSale: true,
             premium: true
 
         }
     },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product + (this.onSale ? " On Sale" : "")
-
-        },
-
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        }
-    },
     methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateVariant(index) {
-            this.selectedVariant = index;
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateCart(id) {
+            this.cart.push(id);
+            if (id == 2234) {
+                this.cart[0] += 1;
+            } else
+                this.cart[1] += 1;
         }
     }
 
