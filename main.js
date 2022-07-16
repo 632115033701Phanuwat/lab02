04b9b70bc5cf7f4ac7bd5064db1e82ae0e727e7c
Phanuwat_emotin <phanuwat_panya@elearning.cmu.ac.th>
2022-07-16 23:54:30 +0700

    4.9

--- main.js
+++ main.js
@@ -4,46 +4,43 @@ const app = Vue.createApp({
             product: 'Shoes',
             brand: 'SE 331',
             //image: './assets/images/socks_green.jpg',
-            //inStock: false,
+            //inStock: true,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
-            selectedVariant:0,
+            selectedVariant: 0,
             cart: 0,
-            onSale: true //3.5
+            onSale: true,
+            premium: true
 
         }
     },
-
     computed: {
         title() {
-            return this.brand + ' ' + this.product 
+            return this.brand + ' ' + this.product + (this.onSale ? " On Sale" : "")
+
         },
+
         image() {
             return this.variants[this.selectedVariant].image
         },
         inStock() {
             return this.variants[this.selectedVariant].quantity
-        },
-        onsale() {
-            return this.brand + 'is on sale ' + this.product 
         }
     },
-
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
-        },
         updateVariant(index) {
             this.selectedVariant = index;
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
-
 
 })
